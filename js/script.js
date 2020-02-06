d8bbdef62d61baee774f8d1cf22837166afcbe1d
Sean R Reid <sean@torchcodelab.com>
2020-02-06 13:41:43 -0500

    Added event handlers for numbers and operators

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ const input = document.querySelector('#input'), // input/output button
   clear = document.querySelector('#clear'); // clear button
 
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
+
+numbers.forEach(function(number) {
+  number.addEventListener('click', function(e) {
+    input.innerHTML += number.innerText;
+  });
+});
+
+operators.forEach(function(operator) {
+  operator.addEventListener('click', function(e) {
+    input.innerHTML += operator.innerText;
+  });
+});
+
+result.addEventListener('click', function(e) {});
+
+clear.addEventListener('click', function(e) {
+  console.log('clear has been clicked');
+  input.innerHTML = '';
+  resultDisplayed = false;
+});
