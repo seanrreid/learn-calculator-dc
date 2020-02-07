02da2d8b39115d553b89ae8039875727c31953d4
Karley Petracca <karleypetracca@gmail.com>
2020-02-07 17:05:24 -0500

    Fixed decimal concatenating issues

--- js/script.js
+++ js/script.js
@@ -12,52 +12,117 @@ const numberArray = [], operatorArray = [] // built in for each loops further do
 
 let inputArray = []
 
+
+
 // Functions
 
 function addToInput(buttonValue) {
-  let lastInput = input.innerHTML[(input.innerHTML.length)-1]
+  let lastInput = input.innerHTML[input.innerHTML.length-1];
+  let lastArrayEntry = inputArray[(inputArray.length)-1] || "";
+  let buttonText = buttonValue.innerHTML;
   switch (true) {
-    case (operatorArray.indexOf(buttonValue.innerHTML)>=0):
-      inputArray.push(buttonValue.innerHTML)
+    case (operatorArray.indexOf(buttonText)>=0):
+      inputArray.push(buttonText);
+      input.innerHTML += buttonText;
+      console.log("case1 " + buttonText)
       break;
-    case (numberArray.indexOf(lastInput) >=0):
-      inputArray[(inputArray.length)-1] += buttonValue.innerHTML;
+    case (buttonText === "."):
+      switch (true) {
+        case (lastArrayEntry.indexOf(".")>=0):
+          console.log("Existing decimal " + buttonText);
+          break;
+        case (operatorArray.indexOf(lastInput)<0):
+          console.log("Adding to number...");
+          inputArray[(inputArray.length)-1] += buttonText;
+          input.innerHTML += buttonText;
+          break;
+        default:
+          console.log("default subcase " + buttonText);
+          inputArray.push(buttonText);
+          input.innerHTML += buttonText;
+      }
+      break;
+    case (numberArray.indexOf(lastInput)>=0):
+      inputArray[(inputArray.length)-1] += buttonText;
+      input.innerHTML += buttonText;
+      console.log("case4 " + buttonText)
+      break;
+    case (buttonValue === clear):
+      console.log("case5 " + buttonText)
       break;
     default:
-      inputArray.push(buttonValue.innerHTML);
+      console.log("caseDefault")
+      inputArray.push(buttonText);
+      input.innerHTML += buttonText;
   }
-  input.innerHTML += buttonValue.innerHTML;
   console.log(inputArray, input.innerHTML);
 }
 
+function operatorCalc(num1, operator, num2) {
+  switch (operator) {
+    case "*":
+      return parseFloat(num1 * num2);
+    case "/":
+      return parseFloat(num1 / num2);
+    case "+":
+      return parseFloat(+num1 + +num2);
+    case "-":
+      return parseFloat(num1 - num2);
+  } 
+}
+
 function evaluateInput() {
-  if (resultDisplayed === true) {
-    console.log(inputArray);
+  while (inputArray.indexOf("*")>=0) {
+    let tempIndex = inputArray.indexOf("*");
+    let operatorResult = operatorCalc(inputArray[tempIndex-1], inputArray[tempIndex], inputArray[tempIndex+1]);
+    inputArray.splice(tempIndex-1, 3, operatorResult)
   }
+  while (inputArray.indexOf("/")>=0) {
+    let tempIndex = inputArray.indexOf("/");
+    let operatorResult = operatorCalc(inputArray[tempIndex-1], inputArray[tempIndex], inputArray[tempIndex+1]);
+    inputArray.splice(tempIndex-1, 3, operatorResult)
+  }
+  while (inputArray.indexOf("+")>=0) {
+    let tempIndex = inputArray.indexOf("+");
+    let operatorResult = operatorCalc(inputArray[tempIndex-1], inputArray[tempIndex], inputArray[tempIndex+1]);
+    inputArray.splice(tempIndex-1, 3, operatorResult)
+  }
+  while (inputArray.indexOf("-")>=0) {
+    let tempIndex = inputArray.indexOf("-");
+    let operatorResult = operatorCalc(inputArray[tempIndex-1], inputArray[tempIndex], inputArray[tempIndex+1]);
+    inputArray.splice(tempIndex-1, 3, operatorResult)
+  } 
+  input.innerHTML = inputArray[0];
 }
 
 function clearInput() {
   input.innerHTML = "";
   inputArray.length = 0;
-  resultDisplayed = false;
 }
 
 
-// Click Event Listeners
+// Event Listeners
 
 numbers.forEach(number => {
   number.addEventListener("click", function(event) {
     event.preventDefault();
     console.log(number.innerHTML + " pressed!");
-    addToInput(number);
+    if (resultDisplayed === true) {
+      clearInput();
+      addToInput(number);
+      resultDisplayed = false;
+    } else {
+      addToInput(number);
+    }
   })
-  numberArray.push(number.innerHTML)
+  numberArray.push(number.innerHTML);
 })
 
 operators.forEach(operator => {
   operator.addEventListener("click", function(event) {
     event.preventDefault();
-    let lastInput = input.innerHTML[(input.innerHTML.length)-1]
+    let lastInput = input.innerHTML[(input.innerHTML.length)-1];
+    resultDisplayed = false;
     switch(true) {
       case (input.innerHTML === ""):
         console.log("Cannot start with an operator");
@@ -81,10 +146,12 @@ result.addEventListener("click", function(event) {
   console.log(result.innerHTML + " pressed!");
   resultDisplayed = true;
   evaluateInput();
+  console.log("Result is " + input.innerHTML);
 })
 
 clear.addEventListener("click", function(event) {
   event.preventDefault();
   console.log(clear.innerHTML + " pressed!");
   clearInput();
+  resultDisplayed = false;
 })
